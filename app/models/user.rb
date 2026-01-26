6a8848b887f4a0cec0ab942214b0ca9aa6c913ec
popopoom? <caijiayuantian0@gmail.com>
2026-01-26 19:28:22 +0900

    New registration function

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  # Associations
-  has_many :goods
-  has_many :purchases
-
+  
   # Validations
-  validates :email, presence: true, uniqueness: true
-  validates :encrypted_password, presence: true
   validates :nickname, presence: true
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :date_of_birth, presence: true
+  
+  # Associations
+  has_many :goods, dependent: :destroy
+  has_many :purchases, dependent: :destroy
+
 end
