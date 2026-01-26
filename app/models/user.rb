e826326e8db2745d6d61468078382de2720ee174
popopoom? <caijiayuantian0@gmail.com>
2026-01-26 19:42:51 +0900

    Password Validation-Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   # Validations
+  VALID_PASSWORD_REGEX = /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/
+
+  validates :password,
+    format: {
+      with: VALID_PASSWORD_REGEX,
+    },
+    allow_nil: true
+
   validates :nickname, presence: true
   validates :last_name, presence: true
   validates :first_name, presence: true
