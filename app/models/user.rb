2dafab0305bb16437412da2ef958b9c7f5a9d023
popopoom? <caijiayuantian0@gmail.com>
2026-01-26 20:34:48 +0900

    Newly registered validation model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,18 +6,26 @@ class User < ApplicationRecord
   
   # Validations
   VALID_PASSWORD_REGEX = /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/
+  VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/
 
   validates :password,
     format: {
       with: VALID_PASSWORD_REGEX,
     },
     allow_nil: true
-
+  
+  
   validates :nickname, presence: true
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
+
+  validates :last_name_kana, :first_name_kana,
+    format: {
+      with: VALID_KANA_REGEX,
+    }
+
   validates :date_of_birth, presence: true
   
   # Associations
