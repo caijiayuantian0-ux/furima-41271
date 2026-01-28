36c5e7d06d8d924a2b8921d3f8b49967a5ed1d8c
popopoom? <caijiayuantian0@gmail.com>
2026-01-28 16:05:14 +0900

    Introducing-ActiveHash

--- app/models/good.rb
+++ app/models/good.rb
@@ -1,5 +1,19 @@
 class Good < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  
   belongs_to :user
   has_one :purchase
   has_one_attached :image
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :days_to_ship
+  
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+
+  validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :days_to_ship_id,
+            numericality: { other_than: 1, message: "can't be blank" }
 end
