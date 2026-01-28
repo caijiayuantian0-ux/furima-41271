c9766467ef94cbcbb8479b7cbeab49056dfbd590
popopoom? <caijiayuantian0@gmail.com>
2026-01-28 20:53:21 +0900

    Goods model validation

--- app/models/good.rb
+++ app/models/good.rb
@@ -1,6 +1,34 @@
 class Good < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  
+  # Validations
+  VALID_PRICE_REGEX = /\A[0-9]+\z/
+
+
+  validates :image, presence: true
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :price, presence: true
+
+
+  with_options numericality: { other_than: 1, message: "を選択してください" } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :days_to_ship_id
+  end
+
+  validates :price,
+            format: { 
+              with: VALID_PRICE_REGEX
+            },
+            numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 300,
+              less_than_or_equal_to: 9_999_999
+            }
+
+  # Associations
   belongs_to :user
   has_one :purchase
   has_one_attached :image
@@ -9,11 +37,4 @@ class Good < ApplicationRecord
   belongs_to :shipping_fee
   belongs_to :prefecture
   belongs_to :days_to_ship
-  
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :price, presence: true
-
-  validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :days_to_ship_id,
-            numericality: { other_than: 1, message: "can't be blank" }
 end
