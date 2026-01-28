2b7cca5a653beaeee6dc48961eebd38ce1da630c
popopoom? <caijiayuantian0@gmail.com>
2026-01-28 22:18:34 +0900

    DB save

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,7 +1,6 @@
 class GoodsController < ApplicationController
 
   def index
-    
   end
 
   def new
@@ -9,20 +8,33 @@ class GoodsController < ApplicationController
   end
 
   def create
+  # パラメータの内容を確認
+  puts "====== 全パラメータ ======"
+  puts params.inspect
+  puts "========================="
+  
+  @good = Good.new(goods_params)
+  
+  if @good.save
+    redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
   end
+end
 
   private 
 
   def goods_params
-    params.require(:goods).permit(:name,
-    :description,
-    :category_id,
-    :condition_id,
-    :shipping_fee_id,
-    :prefecture_id,
-    :days_to_ship_id,
-    :price,
-    :image
+    params.require(:good).permit(
+      :name,
+      :description,
+      :category_id,
+      :condition_id,
+      :shipping_fee_id,
+      :prefecture_id,
+      :days_to_ship_id,
+      :price,
+      :image
     ).merge(user_id: current_user.id)
   end
 end
