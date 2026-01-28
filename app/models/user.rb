bc4433089c75b5472e726df759d38a0d37b47a61
popopoom? <caijiayuantian0@gmail.com>
2026-01-28 10:37:18 +0900

    Validation Settings Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   # Validations
   VALID_PASSWORD_REGEX = /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
   VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/
 
   validates :password,
@@ -20,6 +21,11 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
 
+  validates :last_name, :first_name,
+            format: {
+              with: VALID_NAME_REGEX
+            }
+
   validates :last_name_kana, :first_name_kana,
             format: {
               with: VALID_KANA_REGEX
@@ -27,7 +33,4 @@ class User < ApplicationRecord
 
   validates :date_of_birth, presence: true
 
-  # Associations
-  has_many :goods, dependent: :destroy
-  has_many :purchases, dependent: :destroy
 end
