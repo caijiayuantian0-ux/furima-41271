4c19df5e3eee225d037c49585e84e5de68b6876e
popopoom? <caijiayuantian0@gmail.com>
2026-01-29 16:30:55 +0900

    Goods model test code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_01_28_052500) do
+ActiveRecord::Schema[7.1].define(version: 2026_01_28_051604) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -52,11 +52,6 @@ ActiveRecord::Schema[7.1].define(version: 2026_01_28_052500) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_goods_on_user_id"
-  end
-
-  create_table "items", charset: "utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
