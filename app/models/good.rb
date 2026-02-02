e2fad9e4d1e2a3872d80caf01c74872fc54d7a02
popopoom? <caijiayuantian0@gmail.com>
2026-02-02 13:34:15 +0900

    Code review revision model

--- app/models/good.rb
+++ app/models/good.rb
@@ -27,8 +27,8 @@ class Good < ApplicationRecord
             }
 
   # Associations
-  belongs_to :user
-  has_one :purchase
+  ## belongs_to :user
+  ## has_one :purchase
   has_one_attached :image
   belongs_to :category
   belongs_to :condition
