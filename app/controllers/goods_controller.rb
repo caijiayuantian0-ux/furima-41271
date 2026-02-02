06fedbf0142fce1587a7eba696605270c41d9589
popopoom? <caijiayuantian0@gmail.com>
2026-02-02 11:59:44 +0900

    List view

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,8 +1,9 @@
 class GoodsController < ApplicationController
-before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
     @goods = Good.includes(:user).order(created_at: :desc)
+    @goods = Good.order(created_at: :desc)
   end
 
   def new
@@ -10,16 +11,16 @@ before_action :authenticate_user!, only: [:new, :create]
   end
 
   def create
-  @good = Good.new(goods_params)
-  
-  if @good.save
-    redirect_to root_path
-  else
-    render :new, status: :unprocessable_entity
-  end
-end
+    @good = Good.new(goods_params)
 
-  private 
+    if @good.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
 
   def goods_params
     params.require(:good).permit(
