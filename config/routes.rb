17d3eeb59076494e6c6f10ee1d52a8e412aa11b2
popopoom? <caijiayuantian0@gmail.com>
2026-02-02 15:08:35 +0900

    Product branch view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "goods#index"
-  resources :goods, only: [:index, :new, :create, :show]
+  resources :goods, only: [:index, :new, :create, :show, :destroy]
   
 end
