0b3fb75c9a78b28dc6e780a2cd8ee502401054a9
popopoom? <caijiayuantian0@gmail.com>
2026-02-04 15:55:43 +0900

    destroy method controller

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,8 +1,7 @@
 class GoodsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_good, only: [ :edit, :show, :update]
-  before_action :move_to_index, only: [ :edit, :update]
-
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_good, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
   def index
     @goods = Good.includes(:user).order(created_at: :desc)
   end
@@ -36,6 +35,8 @@ class GoodsController < ApplicationController
   end
 
   def destroy
+    @good.destroy
+    redirect_to root_path
   end
   
   private
