8fb97f1187905b8db9254a64f1701d2d7ac649a6
popopoom? <caijiayuantian0@gmail.com>
2026-02-06 19:47:25 +0900

    Form Object Model

---
+++ app/models/purchase_form.rb
@@ -0,0 +1,32 @@
+class Purchase
+  include ActiveModel::Model
+  
+  attr_accessor :user_id, :good_id, 
+                :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number
+  
+    validates :postal_code,  presence: true, format: { with: /\A\d{3}-\d{4}\z/ }
+    validates :prefecture_id, presence: true, numericality: { other_than: 1, }
+    validates :city, :street_address ,  presence: true
+    validates :phone_number,  presence: true, format: { with: /\A\d{10,11}\z/, }
+    validates :user_id, :good_id,  presence: true,
+
+  def save
+    return false unless valid?
+
+    ActiveRecord::Base.transaction do
+      purchase = Purchase.create!(user_id: user_id, good_id: good_id)
+      purchase.create_shipping_address!(
+        postal_code: postal_code,
+        prefecture_id: prefecture_id,
+        city: city,
+        street_address: street_address,
+        building_name: building_name,
+        phone_number: phone_number
+      )
+    end
+
+    true
+  rescue ActiveRecord::RecordInvalid
+    false
+  end
+end
