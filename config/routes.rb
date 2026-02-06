b90f1933d55e5653f7702fab6375aa3792375968
popopoom? <caijiayuantian0@gmail.com>
2026-02-06 20:11:06 +0900

    Add routing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "goods#index"
-  resources :goods, only: [:index, :new, :create, :show, :destroy, :edit, :update]
+  resources :goods, only: [:index, :new, :create, :show, :destroy, :edit, :update] do
+    resources :purchases, only: [:index, :create]
+  end
 end
