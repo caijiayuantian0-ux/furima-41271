da3a908972c8a3f0a1253d5fae00149f83439c1a
popopoom? <caijiayuantian0@gmail.com>
2026-02-07 14:12:45 +0900

    Credit card purchase feature

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-<<<<<<< Updated upstream
+
   const publicKey = gon.public_key
   const payjp = Payjp(publicKey)
   const elements = payjp.elements();
@@ -13,26 +13,6 @@ const pay = () => {
   const form = document.getElementById('charge-form')
   form.addEventListener("submit", (e) => {
     payjp.createToken(numberElement).then(function (response) {
-=======
-  const form = document.getElementById("charge-form");
-  if (!form) return;
-
-  const payjp = Payjp('pk_test_c459a860bfc8af48c1ed8dec');
-  const elements = payjp.elements();
-
-  const numberElement = elements.create("cardNumber");
-  const expiryElement = elements.create("cardExpiry");
-  const cvcElement = elements.create("cardCvc");
-
-  numberElement.mount("#number-form");
-  expiryElement.mount("#expiry-form");
-  cvcElement.mount("#cvc-form");
-
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    payjp.createToken(numberElement).then((response) => {
->>>>>>> Stashed changes
       if (response.error) {
       } else {
         const token = response.id;
@@ -45,15 +25,8 @@ const pay = () => {
       cvcElement.clear();
       document.getElementById("charge-form").submit();
     });
-<<<<<<< Updated upstream
     e.preventDefault();
-=======
->>>>>>> Stashed changes
   });
 };
 
 window.addEventListener("turbo:load", pay);
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
