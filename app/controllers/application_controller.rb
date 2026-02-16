dcf0fd06f3bf0ef4c1739b01115172ac073c4b76
caijiayuantian0-ux <caijiayuantian0@gmail.com>
2026-02-16 15:15:06 +0900

    Basic Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USERS"] && password == ENV["BASIC_AUTH_PASSWORDS"]
+      username == 'admin' && password == '2222'
     end
   end
 
